6378643fbc1c5876cca3a108bce0f4e5cd321146
Monish Kuril <157312471+MonishKuril@users.noreply.github.com>
2024-02-06 15:39:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add any JavaScript functionality you need here
+document.getElementById('connectme-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('C.V Downloaded'); // You can replace this with your own logic to handle form submission
+});
