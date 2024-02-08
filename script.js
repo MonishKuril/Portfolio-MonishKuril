359d3483c0a7d89c8d376176e76c28a1838b7222
Monish Kuril <157312471+MonishKuril@users.noreply.github.com>
2024-02-08 16:20:43 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Add any JavaScript functionality you need here
-document.getElementById('connectme-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-    alert('C.V Downloaded'); // You can replace this with your own logic to handle form submission
-});
