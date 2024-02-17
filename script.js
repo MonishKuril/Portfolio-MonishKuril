a89d7404731acdc6e8563fc602ea8d6dbe7a8a4a
Monish Kuril <157312471+MonishKuril@users.noreply.github.com>
2024-02-17 16:47:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById('connectme-form').addEventListener('submit', function(ev
   // Trigger the download
   var downloadLink = document.createElement('a');
   downloadLink.href = document.getElementById('cv-download-btn').getAttribute('href');
-  downloadLink.download = 'https://github.com/MonishKuril/Portfolio-MonishKuril/blob/main/MK.Resume.pdf';
+  downloadLink.download = 'MK_Resume.pdf';
   downloadLink.click();
 });
 // button go back logic
